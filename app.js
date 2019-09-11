45b5d2a0220e8c15290c349d0142f9938ca3a001
Juan Miguel C. Roman <nhestroman@gmail.com>
2019-09-12 05:32:52 +0800

    Commit 4
    Connected to MySQL

--- app.js
+++ app.js
@@ -5,6 +5,21 @@ const ejs = require('ejs');
 const app = express();
 const port = 3000;
 
+//credentials of the database
+let secret = require('./credentials.json');
+
+const db = mysql.createConnection({
+    host:'localhost',
+    user: secret.user,
+    password: secret.password,
+    database:'inventory'
+});
+
+db.connect((err) => {
+    if(err) throw err;
+    console.log('MySQL succesfully connected!');
+});
+
 app.listen(port,()=> {
     console.log(`Started listening on port ${port}`);
 });
