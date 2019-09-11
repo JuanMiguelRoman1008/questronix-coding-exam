2109984324e81aa50ce700a53979e28507f16bbb
Juan Miguel C. Roman <nhestroman@gmail.com>
2019-09-12 07:30:25 +0800

    Commit 5
    Created home route and index template

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const mysql = require('mysql');
-const ejs = require('ejs');
 
 const app = express();
 const port = 3000;
+
+app.set('view engine','ejs');
+app.use(express.static(__dirname + '/public'));
 
 //credentials of the database
 let secret = require('./credentials.json');
@@ -14,6 +16,13 @@ const db = mysql.createConnection({
     password: secret.password,
     database:'inventory'
 });
+
+//Routes
+app.get('/', (req, res) => {
+    res.render('pages/index');
+});
+
+
 
 db.connect((err) => {
     if(err) throw err;
